0161a7fc58deb9fd10d7d16e57ff9e35f0fe68f5
Tony <anthonytjohnson09@gmail.com>
2024-11-03 08:58:17 -0500

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+
+
+
+<template>
+  <v-app>
+    <v-main class="custom-main">
+      <TextBox />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import './assets/main.css';
+import TextBox from './components/TextBox.vue';
+import HelloWorld from './components/HelloWorld.vue';
+import TheWelcome from './components/TheWelcome.vue';
+</script>
+
+<style scoped>
+.custom-main {
+  /* Remove the excessive padding */
+  padding-left: 0; /* or adjust as needed */
+  display: flex;
+  justify-content: center;
+  align-items: center; 
+  min-height: 100%; 
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+// src/main.js
+import { createApp } from 'vue';
+import App from './App.vue';
+import vuetify from './plugin/vuetify';
+import 'vuetify/styles';
+
+
+const app = createApp(App);
+app.use(vuetify);
+app.mount('#app');
