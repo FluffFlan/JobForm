6ab51dd8cc8154ba102228d132d40e3d0fdf9e5a
Tony <anthonytjohnson09@gmail.com>
2024-11-03 17:38:26 -0500

    Deleted vue3-country-region-selection from project.

--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import vuetify from './plugin/vuetify';
 import 'vuetify/styles';
-import vueCountryRegionSelect from './plugin/vueCountryRegionSelect';
+
 
 
 const app = createApp(App);
 app.use(vuetify);
-app.use(vueCountryRegionSelect);
 app.mount('#app');
--- src/plugin/vueCountryRegionSelect.js
+++
@@ -1,4 +0,0 @@
-import vue3CountryRegionSelect from 'vue3-country-region-select';
-
-
-export default vue3CountryRegionSelect;
